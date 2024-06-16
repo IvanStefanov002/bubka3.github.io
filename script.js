96ef0b2c2949f1c669644a850a8f19604d251bb8
IvanStefanov002 <91614351+IvanStefanov002@users.noreply.github.com>
2024-06-16 21:09:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function() {
     const questionTypeSelect = document.getElementById('questionType');
     const freeTextSection = document.getElementById('freeTextSection');
@@ -11,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const questionDisplay = document.getElementById('questionDisplay');
     const userAnswer = document.getElementById('userAnswer');
     const submitAnswerButton = document.getElementById('submitAnswer');
-    const feedback = document.getElementById('feedback');
     const feedbackQuestion = document.getElementById('feedbackQuestion');
     const previousButton = document.getElementById('previousButton');
     const nextButton = document.getElementById('nextButton');
@@ -91,11 +89,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // manageQuestionsButton.addEventListener('click', function() {
-    //     homePage.classList.add('hidden');
-    //     questionSection.classList.remove('hidden');
-    // });
-
     playButton.addEventListener('click', function() {
         homePage.classList.add('hidden');
         questionSection.classList.add('hidden');
@@ -104,33 +97,17 @@ document.addEventListener("DOMContentLoaded", function() {
         displayQuestion();
     });
 
-    // submitAnswerButton.addEventListener('click', function() {
-    //     const currentQuestion = questions[currentQuestionIndex];
-    //     if (currentQuestion.type === 'free-text') {
-    //         if (userAnswer.value.toLowerCase() === currentQuestion.answer.toLowerCase()) {
-    //             feedback.textContent = 'Правилно!';
-    //         } else {
-    //             feedback.textContent = 'Грешно! Правилният отговор е: ' + currentQuestion.answer;
-    //         }
-    //     } else {
-    //         if (userAnswer.value.toLowerCase() === currentQuestion.correctOption) {
-    //             feedback.textContent = 'Правилно!';
-    //         } else {
-    //             feedback.textContent = 'Грешно! Правилният отговор е:: ' + currentQuestion.correctOption.toUpperCase();
-    //         }
-    //     }
-    //     // currentQuestionIndex++;
-    //     // if (currentQuestionIndex < questions.length) {
-    //     //     setTimeout(displayQuestion, 2000);
-    //     // } else {
-    //     //     feedback.textContent += ' You have completed all the questions.';
-    //     //     setTimeout(function() {
-    //     //         playSection.classList.add('hidden');
-    //     //         homePage.classList.remove('hidden');
-    //     //     }, 13000);
-    //     // }
-    //     userAnswer.value = '';
-    // });
+    submitAnswerButton.addEventListener('click', function() {
+        const currentQuestion = questions[currentQuestionIndex];
+        let isCorrect;
+        if (currentQuestion.type === 'free-text') {
+            isCorrect = (userAnswer.value.toLowerCase() === currentQuestion.answer.toLowerCase());
+        } else {
+            isCorrect = (userAnswer.value.toLowerCase() === currentQuestion.correctOption);
+        }
+        showFeedback(isCorrect);
+        userAnswer.value = '';
+    });
 
     function displayQuestion() {
         const currentQuestion = questions[currentQuestionIndex];
@@ -149,18 +126,109 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // previousButton.addEventListener('click', function() {
-    //     if (currentQuestionIndex > 0) {
-    //         currentQuestionIndex--;
-    //         displayQuestion();
-    //     }
-    // });
+    previousButton.addEventListener('click', function() {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            displayQuestion();
+        }
+    });
     
-    // nextButton.addEventListener('click', function() {
-    //     if (currentQuestionIndex < questions.length - 1) {
-    //         currentQuestionIndex++;
-    //         displayQuestion();
-    //     }
-    // });
-});
+    nextButton.addEventListener('click', function() {
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            displayQuestion();
+        }
+    });
 
+    document.getElementById('uploadButton').addEventListener('click', () => {
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+    
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const xml = event.target.result;
+                parseXMLFile(xml);
+                alert('Готово е файлчето буб!');
+            };
+            reader.readAsText(file);
+        } else {
+            alert('Избери първо файлче буб.');
+        }
+    });
+
+    function parseXMLFile(xml) {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xml, 'application/xml');
+    
+        const exams = xmlDoc.getElementsByTagName('Exam');
+        for (let i = 0; i < exams.length; i++) {
+            const questionText = exams[i].getElementsByTagName('Question')[0].textContent;
+            const answers = exams[i].getElementsByTagName('Answers')[0];
+    
+            if (answers.children.length === 1) {
+                // Free-text question
+                const answerText = answers.children[0].textContent;
+                let question = {
+                    type: 'free-text',
+                    question: questionText,
+                    answer: answerText.startsWith('*') ? answerText.substring(1) : answerText // Remove asterisk if present
+                };
+                questions.push(question);
+            } else {
+                // Multiple-choice question
+                const options = {};
+                let correctOption = '';
+    
+                for (let j = 0; j < answers.children.length; j++) {
+                    let answerText = answers.children[j].textContent;
+                    if (answerText.startsWith('*')) {
+                        answerText = answerText.substring(1); // remove the asterisk
+                        correctOption = String.fromCharCode(97 + j); // convert to a, b, c, or d
+                    }
+                    options[String.fromCharCode(97 + j)] = answerText; // a, b, c, d
+                }
+    
+                let question = {
+                    type: 'multiple-choice',
+                    question: questionText,
+                    options: options,
+                    correctOption: correctOption
+                };
+    
+                questions.push(question);
+            }
+        }
+        localStorage.setItem('questions', JSON.stringify(questions));
+    }
+    
+
+    // Modal related functions
+    function showFeedback(isCorrect) {
+        const currentQuestion = questions[currentQuestionIndex];
+        const modal = document.querySelector('.modal');
+        const feedbackImage = document.getElementById('feedbackImage');
+        const feedbackMessage = document.getElementById('feedbackMessage');
+
+        if (isCorrect) {
+            feedbackImage.src = 'images/right.gif'; // Set the path to the correct image
+            feedbackMessage.textContent = "Правилно буб!";
+            modal.style.display = 'block'; // Display the modal
+        } else {
+            feedbackImage.src = 'images/wrong.gif'; // Set the path to the incorrect image
+            if(currentQuestion.type === 'free-text'){
+                feedbackMessage.textContent = 'Грешно буб! Правилният отговор е: ' + currentQuestion.answer;
+            }else{
+                feedbackMessage.textContent = 'Грешно буб! Правилният отговор е: ' + currentQuestion.correctOption.toUpperCase();
+            }
+            modal.style.display = 'block'; // Display the modal
+        }
+
+        // Close the modal when the user clicks anywhere outside of it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        }
+    }
+});
